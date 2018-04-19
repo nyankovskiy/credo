b8d47d66943f5b69536eb6d462f0e722e81444bc
root <root@localhost.localdomain>
2018-04-19 17:37:42 +0000

    Clean forum

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'clancredo';
+$dbuser = 'clancredo';
+$dbpasswd = 'FvRup8kKFr9C3IYy';
+$table_prefix = 'bb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
